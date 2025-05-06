558a1b50716308866a63906dbb0b2b03678d24b2
Thu Nguyen <2973750+telshira@users.noreply.github.com>
2025-05-06 11:50:39 +0700

    Created barebone index.html. Created function for getting computer choice when playing game.

---
+++ script.js
@@ -0,0 +1,5 @@
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  return computerChoice;
+}
