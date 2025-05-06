468e1a6a17ba847a139cc719da0748706aeb8462
Thu Nguyen <2973750+telshira@users.noreply.github.com>
2025-05-06 12:48:11 +0700

    Created global variables for score and selection. Created function to play one round of rock, paper and scissors and changing scores according to the winner.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   let computerChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -5,6 +8,28 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt("Please pick rock, papers, or scissors.");
+  let humanChoice = prompt("Please pick rock, paper, or scissors.");
   return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toUpperCase();
+  computerChoice = computerChoice.toUpperCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if  (humanChoice === "ROCK" && computerChoice === "SCISSORS" 
+    || humanChoice === "PAPER" && computerChoice==="ROCK"
+    || humanChoice === "SCISSORS" && computerChoice ==="PAPER") {
+    humanScore++;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}. Congrats!`);
+  } else {
+    computerScore++;
+    console.log(`You lose!  ${humanChoice} loses to ${computerChoice}. Better luck next time!`);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
